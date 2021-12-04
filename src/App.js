e41fb32b4409222a3b01d91fa0c63f6f8574aa89
skyseerr <mr.ivanasenov@gmail.com>
2021-12-04 10:01:12 +0200

    Add Login page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import Footer from "./components/Footer/Footer";
+import Login from "./components/Login/Login";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
     <Header />
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
     <Footer />
 
