52bda5c1f8ded572b92c33b31cf6b7ba99ac37a5
skyseerr <mr.ivanasenov@gmail.com>
2021-12-04 23:21:13 +0200

    Add Link/to in slider.js

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,186 +1,188 @@
+import { Link } from "react-router-dom";
+
 const Slider = () => {
 
     return (
-        <section class="home home--bg">
-		<div class="container">
-			<div class="row">
-				<div class="col-12">
-					<h1 class="home__title"><b>NEW ITEMS</b> OF THIS SEASON</h1>
+        <section className="home home--bg">
+		<div className="container">
+			<div className="row">
+				<div className="col-12">
+					<h1 className="home__title"><b>NEW ITEMS</b> OF THIS SEASON</h1>
 
-					<button class="home__nav home__nav--prev" type="button">
-						<i class="icon ion-ios-arrow-round-back"></i>
+					<button className="home__nav home__nav--prev" type="button">
+						<i className="icon ion-ios-arrow-round-back"></i>
 					</button>
-					<button class="home__nav home__nav--next" type="button">
-						<i class="icon ion-ios-arrow-round-forward"></i>
+					<button className="home__nav home__nav--next" type="button">
+						<i className="icon ion-ios-arrow-round-forward"></i>
 					</button>
 				</div>
 
-				<div class="col-12">
-					<div class="owl-carousel home__carousel home__carousel--default">
-						<div class="card card--big">
-							<div class="card__cover">
+				<div className="col-12">
+					<div className="owl-carousel home__carousel home__carousel--default">
+						<div className="card card--big">
+							<div className="card__cover">
 								<img src="img/covers/cover.jpg" alt="" />
-								<a href="details.html" class="card__play">
-									<i class="icon ion-ios-play"></i>
-								</a>
-								<span class="card__rate card__rate--green">8.4</span>
+								<Link to="details.html" className="card__play">
+									<i className="icon ion-ios-play"></i>
+								</Link>
+								<span className="card__rate card__rate--green">8.4</span>
 							</div>
-							<div class="card__content">
-								<h3 class="card__title"><a href="details.html">I Dream in Another Language</a></h3>
-								<span class="card__category">
-									<a href="#">Action</a>
-									<a href="#">Triler</a>
+							<div className="card__content">
+								<h3 className="card__title"><Link to="details.html">I Dream in Another Language</Link></h3>
+								<span className="card__category">
+									<Link to="#">Action</Link>
+									<Link to="#">Triler</Link>
 								</span>
 							</div>
 						</div>
 
-						<div class="card card--big">
-							<div class="card__cover">
+						<div className="card card--big">
+							<div className="card__cover">
 								<img src="img/covers/cover2.jpg" alt="" />
-								<a href="details.html" class="card__play">
-									<i class="icon ion-ios-play"></i>
-								</a>
-								<span class="card__rate card__rate--green">7.1</span>
+								<Link to="details.html" className="card__play">
+									<i className="icon ion-ios-play"></i>
+								</Link>
+								<span className="card__rate card__rate--green">7.1</span>
 							</div>
-							<div class="card__content">
-								<h3 class="card__title"><a href="details.html">Benched</a></h3>
-								<span class="card__category">
-									<a href="#">Comedy</a>
+							<div className="card__content">
+								<h3 className="card__title"><Link to="details.html">Benched</Link></h3>
+								<span className="card__category">
+									<Link to="#">Comedy</Link>
 								</span>
 							</div>
 						</div>
 
-						<div class="card card--big">
-							<div class="card__cover">
+						<div className="card card--big">
+							<div className="card__cover">
 								<img src="img/covers/cover3.jpg" alt="" />
-								<a href="details.html" class="card__play">
-									<i class="icon ion-ios-play"></i>
-								</a>
-								<span class="card__rate card__rate--green">7.9</span>
+								<Link to="details.html" className="card__play">
+									<i className="icon ion-ios-play"></i>
+								</Link>
+								<span className="card__rate card__rate--green">7.9</span>
 							</div>
-							<div class="card__content">
-								<h3 class="card__title"><a href="details.html">Whitney</a></h3>
-								<span class="card__category">
-									<a href="#">Romance</a>
-									<a href="#">Drama</a>
+							<div className="card__content">
+								<h3 className="card__title"><Link to="details.html">Whitney</Link></h3>
+								<span className="card__category">
+									<Link to="#">Romance</Link>
+									<Link to="#">Drama</Link>
 								</span>
 							</div>
 						</div>
 
-						<div class="card card--big">
-							<div class="card__cover">
+						<div className="card card--big">
+							<div className="card__cover">
 								<img src="img/covers/cover4.jpg" alt="" />
-								<a href="details.html" class="card__play">
-									<i class="icon ion-ios-play"></i>
-								</a>
-								<span class="card__rate card__rate--yellow">6.8</span>
+								<Link to="details.html" className="card__play">
+									<i className="icon ion-ios-play"></i>
+								</Link>
+								<span className="card__rate card__rate--yellow">6.8</span>
 							</div>
-							<div class="card__content">
-								<h3 class="card__title"><a href="details.html">Blindspotting</a></h3>
-								<span class="card__category">
-									<a href="#">Comedy</a>
-									<a href="#">Drama</a>
+							<div className="card__content">
+								<h3 className="card__title"><Link to="details.html">Blindspotting</Link></h3>
+								<span className="card__category">
+									<Link to="#">Comedy</Link>
+									<Link to="#">Drama</Link>
 								</span>
 							</div>
 						</div>
 
-						<div class="card card--big">
-							<div class="card__cover">
+						<div className="card card--big">
+							<div className="card__cover">
 								<img src="img/covers/cover5.jpg" alt="" />
-								<a href="details.html" class="card__play">
-									<i class="icon ion-ios-play"></i>
-								</a>
-								<span class="card__rate card__rate--yellow">6.2</span>
+								<Link to="details.html" className="card__play">
+									<i className="icon ion-ios-play"></i>
+								</Link>
+								<span className="card__rate card__rate--yellow">6.2</span>
 							</div>
-							<div class="card__content">
-								<h3 class="card__title"><a href="details.html">Blindspotting</a></h3>
-								<span class="card__category">
-									<a href="#">Comedy</a>
-									<a href="#">Drama</a>
+							<div className="card__content">
+								<h3 className="card__title"><Link to="details.html">Blindspotting</Link></h3>
+								<span className="card__category">
+									<Link to="#">Comedy</Link>
+									<Link to="#">Drama</Link>
 								</span>
 							</div>
 						</div>
 
-						<div class="card card--big">
-							<div class="card__cover">
+						<div className="card card--big">
+							<div className="card__cover">
 								<img src="img/covers/cover6.jpg" alt="" />
-								<a href="details.html" class="card__play">
-									<i class="icon ion-ios-play"></i>
-								</a>
-								<span class="card__rate card__rate--red">5.3</span>
+								<Link to="details.html" className="card__play">
+									<i className="icon ion-ios-play"></i>
+								</Link>
+								<span className="card__rate card__rate--red">5.3</span>
 							</div>
-							<div class="card__content">
-								<h3 class="card__title"><a href="details.html">I Dream in Another Language</a></h3>
-								<span class="card__category">
-									<a href="#">Action</a>
-									<a href="#">Triler</a>
+							<div className="card__content">
+								<h3 className="card__title"><Link to="details.html">I Dream in Another Language</Link></h3>
+								<span className="card__category">
+									<Link to="#">Action</Link>
+									<Link to="#">Triler</Link>
 								</span>
 							</div>
 						</div>
 
-						<div class="card card--big">
-							<div class="card__cover">
+						<div className="card card--big">
+							<div className="card__cover">
 								<img src="img/covers/cover7.jpg" alt="" />
-								<a href="details.html" class="card__play">
-									<i class="icon ion-ios-play"></i>
-								</a>
-								<span class="card__rate card__rate--red">5.9</span>
+								<Link to="details.html" className="card__play">
+									<i className="icon ion-ios-play"></i>
+								</Link>
+								<span className="card__rate card__rate--red">5.9</span>
 							</div>
-							<div class="card__content">
-								<h3 class="card__title"><a href="details.html">Benched</a></h3>
-								<span class="card__category">
-									<a href="#">Comedy</a>
+							<div className="card__content">
+								<h3 className="card__title"><Link to="details.html">Benched</Link></h3>
+								<span className="card__category">
+									<Link to="#">Comedy</Link>
 								</span>
 							</div>
 						</div>
 
-						<div class="card card--big">
-							<div class="card__cover">
+						<div className="card card--big">
+							<div className="card__cover">
 								<img src="img/covers/cover8.jpg" alt="" />
-								<a href="details.html" class="card__play">
-									<i class="icon ion-ios-play"></i>
-								</a>
-								<span class="card__rate card__rate--green">9.1</span>
+								<Link to="details.html" className="card__play">
+									<i className="icon ion-ios-play"></i>
+								</Link>
+								<span className="card__rate card__rate--green">9.1</span>
 							</div>
-							<div class="card__content">
-								<h3 class="card__title"><a href="details.html">Whitney</a></h3>
-								<span class="card__category">
-									<a href="#">Romance</a>
-									<a href="#">Drama</a>
+							<div className="card__content">
+								<h3 className="card__title"><Link to="details.html">Whitney</Link></h3>
+								<span className="card__category">
+									<Link to="#">Romance</Link>
+									<Link to="#">Drama</Link>
 								</span>
 							</div>
 						</div>
 
-						<div class="card card--big">
-							<div class="card__cover">
+						<div className="card card--big">
+							<div className="card__cover">
 								<img src="img/covers/cover9.jpg" alt="" />
-								<a href="details.html" class="card__play">
-									<i class="icon ion-ios-play"></i>
-								</a>
-								<span class="card__rate card__rate--yellow">6.6</span>
+								<Link to="details.html" className="card__play">
+									<i className="icon ion-ios-play"></i>
+								</Link>
+								<span className="card__rate card__rate--yellow">6.6</span>
 							</div>
-							<div class="card__content">
-								<h3 class="card__title"><a href="details.html">Blindspotting</a></h3>
-								<span class="card__category">
-									<a href="#">Comedy</a>
-									<a href="#">Drama</a>
+							<div className="card__content">
+								<h3 className="card__title"><Link to="details.html">Blindspotting</Link></h3>
+								<span className="card__category">
+									<Link to="#">Comedy</Link>
+									<Link to="#">Drama</Link>
 								</span>
 							</div>
 						</div>
 
-						<div class="card card--big">
-							<div class="card__cover">
+						<div className="card card--big">
+							<div className="card__cover">
 								<img src="img/covers/cover10.jpg" alt="" />
-								<a href="details.html" class="card__play">
-									<i class="icon ion-ios-play"></i>
-								</a>
-								<span class="card__rate card__rate--green">8.1</span>
+								<Link to="details.html" className="card__play">
+									<i className="icon ion-ios-play"></i>
+								</Link>
+								<span className="card__rate card__rate--green">8.1</span>
 							</div>
-							<div class="card__content">
-								<h3 class="card__title"><a href="details.html">Blindspotting</a></h3>
-								<span class="card__category">
-									<a href="#">Comedy</a>
-									<a href="#">Drama</a>
+							<div className="card__content">
+								<h3 className="card__title"><Link to="details.html">Blindspotting</Link></h3>
+								<span className="card__category">
+									<Link to="#">Comedy</Link>
+									<Link to="#">Drama</Link>
 								</span>
 							</div>
 						</div>
