2b569736ce6620a68cc5c6b89a886b6aa790bf7c
skyseerr <mr.ivanasenov@gmail.com>
2021-12-15 23:52:06 +0200

    Minor fixes in catalog

--- src/components/Catalog/Catalog.js
+++ src/components/Catalog/Catalog.js
@@ -1,11 +1,10 @@
 import { useState, useEffect } from "react";
 
-import { Link } from "react-router-dom";
-
 import { getAllMovies } from "../../services/movieService";
 import Card from "./Card/Card";
 
 import Filter from "../Filter/Filter";
+import BreadCrumbs from "../BreadCrumbs/BreadCrumbs";
 
 const Catalog = () => {
 
@@ -24,23 +23,9 @@ const Catalog = () => {
     return(
       <>
     	<div>
-			<section className="section section--first section--bg" data-bg="img/section/section.jpg">
-			<div className="container">
-				<div className="row">
-					<div className="col-12">
-						<div className="section__wrap">
-							<h1 className="section__title">Catalog</h1>
-		
-							<ul className="breadcrumb">
-								<li className="breadcrumb__item"><Link to={"/"}>Home</Link></li>
-								<li className="breadcrumb__item breadcrumb__item--active">Catalog</li>
-							</ul>
-						</div>
-					</div>
-				</div>
-			</div>
-		</section>
         
+		<BreadCrumbs props={{"name" :"Catalog"}} />
+
         <Filter />
 
 		<div className="catalog">
