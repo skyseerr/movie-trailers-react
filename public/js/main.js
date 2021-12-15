8c9028f8b7ab34b32f6f047c1a1d1bfde08ab6ac
skyseerr <mr.ivanasenov@gmail.com>
2021-12-15 23:50:27 +0200

    Minor fixes in Css

--- public/js/main.js
+++ public/js/main.js
@@ -133,16 +133,16 @@ $(document).ready(function () {
 	/*==============================
 	Section bg
 	==============================*/
-	$('.section--bg, .details__bg').each( function() {
-		if ($(this).attr("data-bg")){
-			$(this).css({
-				'background': 'url(' + $(this).data('bg') + ')',
-				'background-position': 'center center',
-				'background-repeat': 'no-repeat',
-				'background-size': 'cover'
-			});
-		}
-	});
+	// $('.section--bg, .details__bg').each( function() {
+	// 	if ($(this).attr("data-bg")){
+	// 		$(this).css({
+	// 			'background': 'url(' + $(this).data('bg') + ')',
+	// 			'background-position': 'center center',
+	// 			'background-repeat': 'no-repeat',
+	// 			'background-size': 'cover'
+	// 		});
+	// 	}
+	// });
 
 	/*==============================
 	Section carousel
