3b678a6463ce239fb0c900ab788f5c50af6a30b4
skyseerr <mr.ivanasenov@gmail.com>
2021-12-20 23:42:14 +0200

    Make comments re-render after comment

--- src/components/Catalog/Details/Commnets/Comments.js
+++ src/components/Catalog/Details/Commnets/Comments.js
@@ -9,108 +9,171 @@ import YouMayAlsoLike from "../YouMayAlsoLike/YouMayAlsoLike";
 
 import "./Comments.css";
 
-
 const Comments = (genre) => {
-    
-    const [comments, setComments] = useState([]);
-    const [cookies, setCookie, removeCookie] = useCookies(["jwtToken"]);
-    const {movieId} = useParams()
-    const onSubmit = (e) => {
-        e.preventDefault()
-        
-        let formData = new FormData(e.currentTarget);
-        
-        let title = formData.get('title');
-        let review = formData.get('review');
-        let userName = localStorage.getItem('user');
-        
-        create({title, review, userName}, cookies["jwtToken"], movieId, userName);
+  const [comments, setComments] = useState([]);
+  const [cookies, setCookie, removeCookie] = useCookies(["jwtToken"]);
+  const { movieId } = useParams();
+  const [isCommentCreated, setIsCommentCreated] = useState(true);
+  const onSubmit = (e) => {
+    e.preventDefault();
 
-        e.currentTarget.title.value = '';
-        e.currentTarget.review.value = '';
+    let formData = new FormData(e.currentTarget);
+
+    let title = formData.get("title");
+    let review = formData.get("review");
+    let userName = localStorage.getItem("user");
+
+    create({ title, review, userName }, cookies["jwtToken"], movieId, userName);
+    setIsCommentCreated(true);
+    e.currentTarget.title.value = "";
+    e.currentTarget.review.value = "";
+  };
+
+  useEffect(() => {
+    if (isCommentCreated) {
+      getAllComments(movieId)
+        .then((result) => {
+          setComments(result);
+          setIsCommentCreated(false);
+        })
+        .catch((err) => {
+          console.log(err.error);
+        });
     }
+  }, [isCommentCreated]);
 
+  return (
+    <>
+      <section className="content">
+        <div className="content__head">
+          <div className="container">
+            <div className="row">
+              <div className="col-12">
+                <h2 className="content__title">Comments:</h2>
 
-    useEffect(() => {
-		getAllComments(movieId)
-		  .then(result =>{
-			setComments(result);
-		  })
-		  .catch(err=> {
-			  console.log(err.error);
-		  })
-		}, [comments]);
+                <div className="content__mobile-tabs" id="content__mobile-tabs">
+                  <div
+                    className="content__mobile-tabs-btn dropdown-toggle"
+                    role="navigation"
+                    id="mobile-tabs"
+                    data-toggle="dropdown"
+                    aria-haspopup="true"
+                    aria-expanded="false"
+                  >
+                    <input type="button" value="Comments" />
+                    <span></span>
+                  </div>
 
+                  <div
+                    className="content__mobile-tabs-menu dropdown-menu"
+                    aria-labelledby="mobile-tabs"
+                  >
+                    <ul className="nav nav-tabs" role="tablist">
+                      <li className="nav-item">
+                        <a
+                          className="nav-link active"
+                          id="1-tab"
+                          data-toggle="tab"
+                          href="#tab-1"
+                          role="tab"
+                          aria-controls="tab-1"
+                          aria-selected="true"
+                        >
+                          Comments
+                        </a>
+                      </li>
 
-    return(
-      
-<>
-	<section className="content">
-		<div className="content__head">
-			<div className="container">
-				<div className="row">
-					<div className="col-12">
-						<h2 className="content__title">Comments:</h2>
+                      <li className="nav-item">
+                        <a
+                          className="nav-link"
+                          id="2-tab"
+                          data-toggle="tab"
+                          href="#tab-2"
+                          role="tab"
+                          aria-controls="tab-2"
+                          aria-selected="false"
+                        >
+                          Reviews
+                        </a>
+                      </li>
 
-						<div className="content__mobile-tabs" id="content__mobile-tabs">
-							<div className="content__mobile-tabs-btn dropdown-toggle" role="navigation" id="mobile-tabs" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-								<input type="button" value="Comments" />
-								<span></span>
-							</div>
+                      <li className="nav-item">
+                        <a
+                          className="nav-link"
+                          id="3-tab"
+                          data-toggle="tab"
+                          href="#tab-3"
+                          role="tab"
+                          aria-controls="tab-3"
+                          aria-selected="false"
+                        >
+                          Photos
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
 
-							<div className="content__mobile-tabs-menu dropdown-menu" aria-labelledby="mobile-tabs">
-								<ul className="nav nav-tabs" role="tablist">
-									<li className="nav-item"><a className="nav-link active" id="1-tab" data-toggle="tab" href="#tab-1" role="tab" aria-controls="tab-1" aria-selected="true">Comments</a></li>
+        <div className="container">
+          <div className="row">
+            <div className="col-12 col-lg-8 col-xl-8">
+              <div className="tab-content">
+                <div
+                  className="tab-pane fade show active"
+                  id="tab-1"
+                  role="tabpanel"
+                  aria-labelledby="1-tab"
+                >
+                  <div className="row">
+                    <div className="col-12">
+                      <div className="comments">
+                        <ul className="comments__list">
+                          {comments.map((x) => (
+                            <li key={x._id} className="comments__item">
+                              <CommentBox props={x} />{" "}
+                            </li>
+                          ))}
+                        </ul>
 
-									<li className="nav-item"><a className="nav-link" id="2-tab" data-toggle="tab" href="#tab-2" role="tab" aria-controls="tab-2" aria-selected="false">Reviews</a></li>
+                        <form
+                          action="POST"
+                          className="form"
+                          onSubmit={onSubmit}
+                        >
+                          <input
+                            type="text"
+                            className="form__input"
+                            placeholder="Title"
+                            name="title"
+                          />
+                          <textarea
+                            className="form__textarea"
+                            placeholder="Review"
+                            name="review"
+                          />
+                          <button type="submit" className="form__btn">
+                            Send
+                          </button>
+                        </form>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
 
-									<li className="nav-item"><a className="nav-link" id="3-tab" data-toggle="tab" href="#tab-3" role="tab" aria-controls="tab-3" aria-selected="false">Photos</a></li>
-								</ul>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-
-		<div className="container">
-			<div className="row">
-				<div className="col-12 col-lg-8 col-xl-8">
-					<div className="tab-content">
-						<div className="tab-pane fade show active" id="tab-1" role="tabpanel" aria-labelledby="1-tab">
-							<div className="row">
-								<div className="col-12">
-									<div className="comments">
-										<ul className="comments__list">
-
-                                          {comments.map(x =>  <li key={x._id} className="comments__item"><CommentBox props={x} /> </li>)}
-
-
-										</ul>
-
-										<form action="POST" className="form" onSubmit={onSubmit}>
-											<input type="text" className="form__input" placeholder="Title" name="title"/>
-											<textarea className="form__textarea" placeholder="Review" name="review" />
-											<button type="submit" className="form__btn">Send</button>
-										</form>
-									</div>
-								</div>
-							</div>
-						</div>
-
-					</div>
-				</div>
-
-				<div className="col-12 col-lg-4 col-xl-4">
-				<YouMayAlsoLike props={genre} />
-				</div>
-			</div>
-		</div>
-	</section>
-
+            <div className="col-12 col-lg-4 col-xl-4">
+              <YouMayAlsoLike props={genre} />
+            </div>
+          </div>
+        </div>
+      </section>
     </>
-    )
-
-}
+  );
+};
 
 export default Comments;
