cfa2b7620307404b28933f94f05176e8c9a8fa4f
skyseerr <mr.ivanasenov@gmail.com>
2021-12-23 10:49:55 +0200

    Fix errors in console

--- src/components/Catalog/Card/Card.js
+++ src/components/Catalog/Card/Card.js
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import uniqid from 'uniqid';
 
 import "./Card.css";
 
@@ -20,7 +21,7 @@ const Card = ({
             <div className="card__content">
                 <h3 className="card__title"><Link to={`/details/${movie._id}`}>{movie.title}</Link></h3>
                 <span className="card__category">
-                    {movieGenre.map(x => <Link key={x._id} to={`/catalog/category/${x.toLowerCase()}`}>{x}</Link>)}
+                    {movieGenre.map(x => <Link key={uniqid()} to={`/catalog/category/${x.toLowerCase()}`}>{x}</Link>)}
                 </span>
             </div>
         </div>
